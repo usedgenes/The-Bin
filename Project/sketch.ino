3c1baba7f96a63e468074e9e5f6ca0dd7e8f6bdb
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-07-12 13:21:43 -0700

    Edited Files

--- Project/sketch.ino
+++ Project/sketch.ino
@@ -5,47 +5,71 @@
 #include <Adafruit_SSD1306.h>
 #include <Servo.h>
 
-
-
 #define SCREEN_WIDTH        128 // OLED display width, in pixels
 #define SCREEN_HEIGHT        64 // OLED display height, in pixels
 
 Adafruit_MPU6050 mpu;
 sensors_event_t event;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-Servo servo;  // create servo object to control a servo
-
-const unsigned long interval = 40;
-unsigned long previousMillis;
-const int rgbPins[] = { 27, 26, 22};
+Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial1.begin(115200);
-  servo.attach(14);  // attaches the servo on pin 9 to the servo object
-
-  for( int i=0; i<3; i++) {
-    pinMode( rgbPins[i], OUTPUT);
-  }
-
+  Serial.begin(115200);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
+  }
+
+  while (!mpu.begin()) {
+    Serial.println("MPU6050 not connected!");
+    delay(1000);
   }
 
   display.clearDisplay();
   display.setTextSize(1);             
   display.setTextColor(WHITE);        
   display.setCursor(0,10);             
-  display.println("Hello, world!");
+  display.println("Starting");
   display.display();
-  delay(2000); 
+  
+  pinMode(7, INPUT);
+
+  servo.attach(14);
 }
 
 
 void loop() {
-    servo.write(0);
-    tone(21, 250, 1000);
-    analogWrite( rgbPins[0], 1);
-    analogWrite( rgbPins[1], 1);
-    analogWrite( rgbPins[2], 1);
+  int pinValue = digitalRead(7);
+  if(pinValue != LOW) {
+    if(pinValue == HIGH) {
+      mpu.getAccelerometerSensor()->getEvent(&event);
+      display.clearDisplay();
+
+      display.setCursor(0,0);
+      display.print("X: ");
+      display.print(event.acceleration.x);
+      display.println(" m/s^2");
+      display.print("Y: ");
+      display.print(event.acceleration.y);
+      display.println(" m/s^2");
+      display.print("Z: ");
+      display.print(event.acceleration.z);
+      display.println(" m/s^2");
+      display.display();
+      
+      if(event.acceleration.x == 0 && event.acceleration.y == 0 && event.acceleration.z == 0) {
+        tone(21, 440, 1000);
+      }
+      else {
+        if(event.acceleration.x > 0) {
+          servo.write(0);
+        }
+        if(event.acceleration.x < 0) {
+          servo.write(180);
+        }
+      }
+    }
+  }
+
+  delay(100);
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # The-Bin
+
+A simple project using a Raspberry Pico to display MPU6050 accelerometer data on an OLED display. If there is no acceleration, a buzzer plays, and a servo changes position depending on whether acceleration is positive or negative in the x-direction. A button controls the whole program to make adjusting the accelerometer easier. 
